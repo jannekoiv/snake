d68478b387be598789bfaef108ac4f135c1a1835
Janne Koivuranta <jannekoiv@gmail.com>
2015-02-18 18:40:43 +0200

    Replaced magic numbers with segment type constants.

--- src/com/example/hello/Segment.java
+++ src/com/example/hello/Segment.java
@@ -14,6 +14,9 @@ public class Segment
     private Vector2i nextDirection;
     private boolean isFirstSegment;
     private boolean isLastSegment;
+    public static final int SEGMENT_BODY = 0;
+    public static final int SEGMENT_HEAD = 1;
+    public static final int SEGMENT_TAIL = 2;
 
     public Segment(Vector2i position, Vector2i direction)
     {
